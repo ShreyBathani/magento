845814bbf53db182a7a17b33a8eda59fe7b2782a
Shrey Bathani <shreybathani007@gmail.com>
2021-05-27 22:31:56 +0530

    conflict resolved

--- app/code/local/Ccc/Vendor/controllers/Adminhtml/Request/NewController.php
+++ app/code/local/Ccc/Vendor/controllers/Adminhtml/Request/NewController.php
@@ -44,51 +44,56 @@ class Ccc_Vendor_Adminhtml_Request_NewController extends Mage_Adminhtml_Controll
     {
         try {
             $product = $this->_initProduct();
-            $catalogProduct = Mage::getModel('catalog/product');
-            $entityTypeId = Mage::getModel('eav/entity')
-                ->settype(Mage_Catalog_Model_Product::ENTITY)
-                ->getTypeId();
-            $setId = Mage::getModel('eav/entity_setup', 'core_setup')
-                ->getAttributeSetId(Mage_Catalog_Model_Product::ENTITY, 'Default');
-            $catalogProduct->setStoreId(1)
-                ->setWebsiteId([1])
-                ->setAttributeSetId($setId)
-                ->setTypeId('simple')
-                ->setCreatedAt(strtotime('now'))
-                ->setSku($product->getSku())
-                ->setName($product->getName())
-                ->setWeight($product->getWeight())
-                ->setStatus($product->getStatus())
-                ->setTaxClassId(4)
-                ->setVisisbility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
-                ->setPrice($product->getPrice())
-                ->setMrspEnabled(1)
-                ->setMsrpDisplayActualPriceType(1)
-                ->setMrsp(1)
-                ->setDescription($product->getDescription())
-                ->setShortDescription($product->getShortDescription())
-                ->setEntityTypeId($entityTypeId);
-
-            $catalogProduct->setStockData(array(
-                'is_in_stock' => 1, //Stock Availability
-                'qty' => 50 //qty
-                )
-            );
-            $catalogProduct->save();
-
-            $product->setVendorProductRequestStatus(Ccc_Vendor_Model_Product_Request::REQUEST_ADD)
-            ->setVendorProductApproved(Ccc_Vendor_Model_Product_Request::REQUEST_APPROVED)
-            ->setCatalogProductId($catalogProduct->getId());
-            $product->save();
-
-            $request = Mage::getModel('vendor/product_request')
-                ->setVendorId($product->getVendorId())
-                ->setVendorProductId($product->getId())
-                ->setCatalogProductId($catalogProduct->getId())
-                ->setRequestType($product->getVendorProductRequestStatus().'/'.$product->getVendorProductApproved());
-            $request->save();
-
-            $this->_getSession()->addSuccess('New Product successfully saved.');
+            if($product->getVendorProductApproved() == Ccc_Vendor_Model_Product_Request::REQUEST_APPROVED){
+                $this->_getSession()->addSuccess('Product already approved.');
+            }
+            else{
+                $catalogProduct = Mage::getModel('catalog/product');
+                $entityTypeId = Mage::getModel('eav/entity')
+                    ->settype(Mage_Catalog_Model_Product::ENTITY)
+                    ->getTypeId();
+                $setId = Mage::getModel('eav/entity_setup', 'core_setup')
+                    ->getAttributeSetId(Mage_Catalog_Model_Product::ENTITY, 'Default');
+                $catalogProduct->setStoreId(1)
+                    ->setWebsiteId([1])
+                    ->setAttributeSetId($setId)
+                    ->setTypeId('simple')
+                    ->setCreatedAt(strtotime('now'))
+                    ->setSku($product->getSku())
+                    ->setName($product->getName())
+                    ->setWeight($product->getWeight())
+                    ->setStatus($product->getStatus())
+                    ->setTaxClassId(4)
+                    ->setVisisbility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH)
+                    ->setPrice($product->getPrice())
+                    ->setMrspEnabled(1)
+                    ->setMsrpDisplayActualPriceType(1)
+                    ->setMrsp(1)
+                    ->setDescription($product->getDescription())
+                    ->setShortDescription($product->getShortDescription())
+                    ->setEntityTypeId($entityTypeId);
+        
+                $catalogProduct->setStockData(array(
+                    'is_in_stock' => 1, //Stock Availability
+                    'qty' => 50 //qty
+                    )
+                );
+                $catalogProduct->save();
+        
+                $product->setVendorProductRequestStatus(Ccc_Vendor_Model_Product_Request::REQUEST_ADD)
+                    ->setVendorProductApproved(Ccc_Vendor_Model_Product_Request::REQUEST_APPROVED)
+                    ->setCatalogProductId($catalogProduct->getId());
+                $product->save();
+        
+                $request = Mage::getModel('vendor/product_request')
+                    ->setVendorId($product->getVendorId())
+                    ->setVendorProductId($product->getId())
+                    ->setCatalogProductId($catalogProduct->getId())
+                    ->setRequestType($product->getVendorProductRequestStatus().'/'.$product->getVendorProductApproved());
+                $request->save();
+        
+                $this->_getSession()->addSuccess('New Product successfully saved.');
+            }
         } 
         catch (Exception $e) {
             $this->_getSession()->addError($e->getMessage());
